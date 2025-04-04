60cae1c55f1cb896e2f6b0e1fe9937c35dcb172a
Dema123 <gacema426@gmail.com>
2025-04-04 15:58:46 -0400

    commit segment

---
+++ src/SegmentAggre.java
@@ -0,0 +1,14 @@
+public class SegmentAggre {
+    private Point pt1;
+    private Point pt2;
+
+    public SegmentAggre(Point pt1, Point pt2){
+        this.pt1 = pt1;
+        this.pt2 = pt2;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.pt1.toString() + "->" + this.pt2.toString() + "]";
+    }
+}
